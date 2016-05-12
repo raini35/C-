368c262128249af66436490ddc86d9e8957da83e
raini35 <rainier.f.go@gmail.com>
2016-05-11 20:36:02 -0400

    Add files via upload

---
+++ Chapter_9/exOfBin&LinSearch.cpp
@@ -0,0 +1,43 @@
+/*int searchList(const int list[], int size, int value) 
+{
+	int index = 0; 
+	int position = -1; 
+	bool found = false; 
+	
+	while (index < size && !found) 
+	{
+		if(list[index] == value)
+		{
+			found = true; 
+			position = index; 
+		}
+		index ++ 
+	}
+	return position; 
+}
+
+int binarySearch( const int array[], int size, int value) 
+{
+	int first = 0; 
+		last = size - 1; 
+		middle, 
+		position = -1; 
+	bool found = false; 
+	
+	while(!found && first <= last) 
+	{
+		middle = (first + last) / 2; 
+		if (array[middle] == value)
+		{
+			found = true; 
+			position = middle; 
+		}
+		else if (array[middle] > value)
+			last middle - 1; 
+		else
+			first = middle + 1; 
+	}
+	return position; 
+}
+*/
+
