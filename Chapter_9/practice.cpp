c68163ea56395ac009264c0223a7f92b8b2d25be
raini35 <rainier.f.go@gmail.com>
2016-05-14 22:20:02 -0400

    Create practice.cpp

---
+++ Chapter_9/practice.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+#include <cmath> 
+#include <string> 
+#include <vector> 
+#include <cstlib>
+using namespace std; 
+
+/*Classes
+  Loops 
+  Vectors
+  Arrays
+*/
+
+int main(int argc, char *argv[]) 
+{ 
+  int x; 
+  double y; 
+  string z; 
+  return 0; 
+}
