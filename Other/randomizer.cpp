0183d563940a30d8bac9ef5f2cbc510fa47a13df
raini35 <rainier.f.go@gmail.com>
2016-05-16 22:48:40 -0400

    Add files via upload

---
+++ Other/randomizer.cpp
@@ -0,0 +1,27 @@
+#include <iostream> 
+#include <vector> 
+#include <string>
+using namespace std; 
+
+int main() 
+{
+
+	vector<string> editorial; 
+	vector<string> exec; 
+	//Need a way to do it randomly
+	//Two random numbers (one for exec & one for editorial) 
+	//I need to remove it from the vector 
+	//I should probably make a class (the values would be their section & partner)
+	
+	cout << "Time to pick partners: " << endl; 
+	
+	while(editorial.size() != 0 && exec.size() != 0)
+	{
+		
+		
+	}
+	
+	
+
+}
+
